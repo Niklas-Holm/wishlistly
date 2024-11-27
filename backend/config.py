33067294387ca593b097dec35abe3e017175682a
Niklas Holm <niklasholm04@gmail.com>
2024-11-27 02:08:15 +0100

    render internal redis added

--- backend/config.py
+++ backend/config.py
@@ -2,10 +2,11 @@ from dotenv import load_dotenv
 import os
 import redis
 
+# Load environment variables from the .env file
 load_dotenv()
 
 class ApplicationConfig:
-    SECRET_KEY = os.environ["SECRET_KEY"]
+    SECRET_KEY = os.environ.get("SECRET_KEY")  # Get the SECRET_KEY from the .env file
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
@@ -14,6 +15,10 @@ class ApplicationConfig:
     SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.Redis(host='localhost', port=6379)
+
+    # Get the REDIS_URL from the .env file and use it for Redis configuration
+    REDIS_URL = os.environ.get("REDIS_URL")
+    SESSION_REDIS = redis.from_url(REDIS_URL)  # Use redis.from_url to configure Redis from the URL
+
     SESSION_COOKIE_SAMESITE = "None"
     SESSION_COOKIE_SECURE = True
